99eb5e977d5938b403997a52975158b838a492a0
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-04-24 13:42:39 +0530

    Created an basic github repository for the frontend part of the fotos app. Bootstrapped the react app with vite.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-8xl text-[#7e0000] font-extrabold tracking-extrawider">
+      Fotos react app
+    </div>
+  )
+}
+
+export default App
